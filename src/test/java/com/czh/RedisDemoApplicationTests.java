aa64bc588a2f6465bee6cd81a551ce845e3f3ea6
Shox-Chang <992772880@qq.com>
2024-06-18 20:31:06 +0800

    0618

--- src/test/java/com/czh/RedisDemoApplicationTests.java
+++ src/test/java/com/czh/RedisDemoApplicationTests.java
@@ -1,10 +1,14 @@
 package com.czh;
 
+import com.czh.xc.AllResolvedExecutor;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.*;
 import java.util.concurrent.*;
