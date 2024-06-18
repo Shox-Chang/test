7ebb917b5c4178bdf2442d8367bebf6ebe31695c
Shox-Chang <992772880@qq.com>
2024-06-18 22:50:54 +0800

    0618

--- src/test/java/com/czh/RedisDemoApplicationTests.java
+++ src/test/java/com/czh/RedisDemoApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.*;
